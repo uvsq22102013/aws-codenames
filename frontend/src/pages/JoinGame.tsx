78d2efa8d52a4a513bb03504c1bb4cd8fac743b3
uvsq22400951 <rayan.barchi@ens.uvsq.fr>
2025-02-09 22:03:46 +0100

    Ajout de la route pour rejoindre une partie existante

--- frontend/src/pages/JoinGame.tsx
+++ frontend/src/pages/JoinGame.tsx
@@ -4,7 +4,7 @@ export default function HomePage() {
   const [roomCode, setRoomCode] = useState("");
 
   const handleCreateRoom = () => {
-    alert("Nouveau salon créé !");
+    alert("Nouvelle partie créée !");
     //Code pour rediriger vers la nouvelle salle
   };
 
@@ -19,17 +19,17 @@ export default function HomePage() {
 
   return (
     <div className="flex flex-col items-center justify-center h-screen bg-gray-900 text-white">
-      <h1 className="text-3xl font-bold mb-6">Bienvenue sur la plateforme</h1>
+      <h1 className="text-3xl font-bold mb-6">CodeNames</h1>
       <button 
         className="bg-blue-500 hover:bg-blue-600 text-white font-bold py-2 px-4 rounded mb-4"
         onClick={handleCreateRoom}
       >
-        Créer un nouveau salon
+        Créer une nouvelle partie 
       </button>
       <div className="flex flex-col items-center bg-gray-800 p-4 rounded-lg shadow-md">
         <input 
           type="text" 
-          placeholder="Numéro du salon" 
+          placeholder="Numéro de la partie" 
           className="px-3 py-2 border border-gray-600 rounded mb-2 text-black"
           value={roomCode}
           onChange={(e) => setRoomCode(e.target.value)}
@@ -38,7 +38,7 @@ export default function HomePage() {
           className="bg-green-500 hover:bg-green-600 text-white font-bold py-2 px-4 rounded"
           onClick={handleJoinRoom}
         >
-          Intégrer un salon existant
+          Intégrer une partie existante
         </button>
       </div>
     </div>
