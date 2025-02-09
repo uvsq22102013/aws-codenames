fb971022042b6e56008c9c5a36f60dc71a02426c
uvsq22400158 <yanni.doumbia@ens.uvsq.fr>
2025-02-09 17:19:42 +0100

    ajout de verifications lors de l'inscriptions avec meilleur affichage lors d'erreurs

--- backend/src/routes/auth.ts
+++ backend/src/routes/auth.ts
@@ -6,18 +6,32 @@ const router = express.Router();
 
 // Inscription
 router.post("/register", async (req: Request, res: Response): Promise<void> => {
-    const { pseudo, email, mdp } = req.body;
-
-    if (!pseudo || !email || !mdp) {
-        res.status(400).json({ error: "Tous les champs sont requis." });
-        return;
-    }
+    const { pseudo, email, mdp, mdp2 } = req.body;
 
     try {
+        // Vérifier si le mail existe déjà en bdd
+        const existingMail = await prisma.utilisateur.findUnique({
+            where: { email }
+        });
+
+        if (existingMail) {
+            res.status(400).json({ error: "Cet email est déjà utilisé." });
+            return;
+        }
+        const existingUser = await prisma.utilisateur.findUnique({
+            where: { pseudo }
+        });
+
+        if (existingUser) {
+            res.status(400).json({ error: "Ce nom d'utilisateur est déjà utilisé." });
+            return;
+        }
+
         const hashedPassword = await bcrypt.hash(mdp, 10);
         const user = await prisma.utilisateur.create({
             data: { pseudo, email, mdp_hash: hashedPassword },
         });
+
         res.json({ message: "Utilisateur inscrit avec succès", user });
     } catch (error) {
         console.error("Erreur Prisma :", error);
