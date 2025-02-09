21fb1ec8a91db485a4ee1da9fbfc82b1bce9f66c
uvsq22400158 <yanni.doumbia@ens.uvsq.fr>
2025-02-09 18:04:54 +0100

    ajout d'une connexion valide via mail ou pseudo avec petite modif sur l'affichage

--- backend/src/routes/auth.ts
+++ backend/src/routes/auth.ts
@@ -18,6 +18,7 @@ router.post("/register", async (req: Request, res: Response): Promise<void> => {
             res.status(400).json({ error: "Cet email est déjà utilisé." });
             return;
         }
+        // Vérifier si le pseudo existe déjà en bdd
         const existingUser = await prisma.utilisateur.findUnique({
             where: { pseudo }
         });
@@ -38,6 +39,48 @@ router.post("/register", async (req: Request, res: Response): Promise<void> => {
         res.status(500).json({ error: "Erreur d'inscription backend" });
     }
 });
+
+// Connexion
+router.post("/login", async (req: Request, res: Response): Promise<void> => {
+    const { pseudo, email, mdp } = req.body;
+
+    try {
+        // Vérifier si l'utilisateur existe avec ce pseudo
+        const existingPseudo = await prisma.utilisateur.findUnique({
+            where: { pseudo },
+        });
+        // Vérifier si l'utilisateur existe avec cet email
+        const existingMail = await prisma.utilisateur.findUnique({
+            where: { email },
+        });
+
+        if (!existingMail && !existingPseudo) {
+            res.status(400).json({ error: "Login ou mot de passe incorrect." });
+            return;
+        }
+        //donne a user l'objet représentant l'utilisateur
+        let user: any;
+
+        if (existingPseudo) {
+            user = existingPseudo;
+        } else {
+            user = existingMail;
+        }
+        // Vérifier si le mot de passe est correct
+        const isPasswordValid = await bcrypt.compare(mdp, user.mdp_hash);
+
+        if (!isPasswordValid) {
+            res.status(400).json({ error: "Login ou mot de passe incorrect." });
+            return;
+        }
+
+        res.json({ message: "Connexion réussie", user });
+    } catch (error) {
+        console.error("Erreur Prisma :", error);
+        res.status(500).json({ error: "Erreur de connexion backend" });
+    }
+});
+
 
 
 
--- frontend/src/pages/Login.tsx
+++ frontend/src/pages/Login.tsx
@@ -3,22 +3,25 @@ import { useNavigate } from "react-router-dom";
 import axios from "axios";
 
 export default function Login() {
-  const [email, setEmail] = useState("");
+  const [login, setLogin] = useState("");
   const [password, setPassword] = useState("");
+  const [error, setError] = useState(""); // Stocke l'erreur à afficher
   const navigate = useNavigate();
 
   const handleLogin = async () => {
+    setError(""); // Reset de l'erreur
     try {
       const response = await axios.post("http://localhost:3000/api/auth/login", {
-        email,
+        pseudo: login,
+        email: login,
         mdp: password,
       });
 
       localStorage.setItem("token", response.data.token);
       alert("Connexion réussie !");
       navigate("/game");
-    } catch (error) {
-      alert("Erreur de connexion.");
+    } catch (error: any) {
+      setError(error.response?.data?.error || "Erreur de connexion front.");
     }
   };
 
@@ -26,9 +29,10 @@ export default function Login() {
     <div className="min-h-screen flex items-center justify-center bg-gray-200">
       <div className="p-6 bg-white shadow-lg rounded">
         <h2 className="text-2xl font-bold">Connexion</h2>
-        <input type="email" placeholder="Email" className="border p-2 mt-2 w-full" onChange={(e) => setEmail(e.target.value)} />
+        {error && <p className="text-red-500 text-sm text-center mt-2">{error}</p>}
+        <input type="text" placeholder="Pseudo ou Email" className="border p-2 mt-2 w-full" onChange={(e) => setLogin(e.target.value)} />
         <input type="password" placeholder="Mot de passe" className="border p-2 mt-2 w-full" onChange={(e) => setPassword(e.target.value)} />
-        <button onClick={handleLogin} className="bg-blue-500 text-white p-2 mt-4 w-full">Se Connecter</button>
+        <button onClick={handleLogin} className="bg-blue-500 text-white p-2 mt-4 w-full disabled:bg-gray-400" disabled={!login || !password }>Se Connecter</button>
       </div>
     </div>
   );
