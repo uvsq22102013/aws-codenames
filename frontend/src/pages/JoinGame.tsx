a4d34688c747089132e6973396e2b8d500f9854e
Ayoub Boussaid <ayoub.boussaid@ens.uvsq.fr>
2025-02-11 16:58:52 +0100

    merge les branche de yanni et rayan

--- frontend/src/pages/JoinGame.tsx
+++ frontend/src/pages/JoinGame.tsx
@@ -1,77 +1,120 @@
 import { useState } from "react";
+import axios from "axios";
+import { useNavigate } from "react-router-dom";
 
 export default function HomePage() {
-  const [roomCode, setRoomCode] = useState("");
 
-  // Fonction pour créer une nouvelle partie
+
+  const [roomCode, setRoomCode] = useState("");
+    const navigate = useNavigate();
+
+  //fonction pour créer une nouvelle partie
+
   const handleCreateRoom = async () => {
+
     try {
-      // Envoi d'une requête POST pour créer une nouvelle partie
-      const response = await fetch("http://localhost:3000/game", {
-        method: "POST",
-        headers: {
-          "Content-Type": "application/json",
-        },
-        body: JSON.stringify({ createurId: 1 }) //id de l'utilisateur (à modifier avec l'utilisateur réel)
+
+
+      //ici on envoi une requete POST au backend pour créer une partie 
+      const response = await axios.post("http://localhost:3000/api/game/create", {
+
+        createurId: 1, //ID du créateur de la partie est 2 (à modifier plus tard)
+
       });
 
-      if (!response.ok) {
-        throw new Error("Erreur lors de la création de la partie");
-      }
+      //message qui confirme que la partie a bien été créée
+       alert(`Nouvelle partie créée avec succès ! ID: ${response.data.id}`);
+       
+      //on renvoi le joueur vers le lien de la partie
+      navigate(`/game/${response.data.id}`);
 
-      const data = await response.json();
-      alert(`Nouvelle partie créée avec succès ! ID: ${data.id}`);
-      // Rediriger l'utilisateur vers la nouvelle partie (par exemple vers la page de jeu)
-      window.location.href = `/game/${data.id}`;
-
+      
     } catch (error) {
-      alert(error);
+
+//si l'erreur vient d'axios on génère un message d'erreur 
+      if (axios.isAxiosError(error)) {
+
+
+        alert(error.response?.data?.error || " Erreur lors de la création de la partie (Axios)");
+      } else {
+
+//dans le cas contraire 
+        alert("Erreur lors de la création de la partie");
+
+      }
     }
+
+
   };
 
   // Fonction pour rejoindre une partie existante
   const handleJoinRoom = async () => {
+
+// verifie si l'utilisateur entre quelque chose
     if (roomCode.trim() === "") {
+
+
       alert("Veuillez entrer un numéro de partie");
       return;
+
     }
 
     try {
-      // Envoi d'une requête POST pour rejoindre une partie
-      const response = await fetch("http://localhost:3000/join-game", {
-        method: "POST",
-        headers: {
-          "Content-Type": "application/json",
-        },
-        body: JSON.stringify({ roomCode }),
+      // Envoi d'une requête POST au backend avec axios
+
+      const response = await axios.post("http://localhost:3000/api/game/join-game", {
+        roomCode,
+
+
       });
 
-      if (!response.ok) {
-        const errorData = await response.json();
-        alert(errorData.error || "Erreur inconnue");
-        return;
-      }
+      //message dde confirmation qu'on a bien rejoint la partie
+      alert(`Vous avez rejoint la partie ${response.data.game.id}`);
 
-      const data = await response.json();
-      alert(`Vous avez rejoint la partie ${data.game.id}`);
-      // Rediriger l'utilisateur vers la page de la partie
-      window.location.href = `/game/${data.game.id}`;
+
+      //redirige vers la partie
+      window.location.href = `/game/${response.data.game.id}`;
+
+
 
     } catch (error) {
-      alert("Erreur lors de la connexion à la partie");
+
+
+//on vérifie également si l'erreur vient d'axios
+      if (axios.isAxiosError(error)) {
+
+        alert(error.response?.data?.error || "Erreur lors de la connexion à la partie (Axios)");
+
+      } else {
+
+
+        alert("Erreur lors de la connexion à la partie front");
+
+      }
+
     }
   };
 
+
+
+
+  //les lignes qui suive concernent l'interface utilisateiur sur la page d'accueil
+
   return (
     <div className="flex flex-col items-center justify-center h-screen bg-gray-900 text-white">
+
       <h1 className="text-3xl font-bold mb-6">CodeNames</h1>
+
       <button 
         className="bg-blue-500 hover:bg-blue-600 text-white font-bold py-2 px-4 rounded mb-4"
         onClick={handleCreateRoom}
       >
-        Créer une nouvelle partie 
+        Creer une nouvelle partie 
       </button>
+
+
       <div className="flex flex-col items-center bg-gray-800 p-4 rounded-lg shadow-md">
+
         <input 
           type="text" 
           placeholder="Numéro de la partie" 
@@ -79,6 +122,8 @@ export default function HomePage() {
           value={roomCode}
           onChange={(e) => setRoomCode(e.target.value)}
         />
+
+
         <button 
           className="bg-green-500 hover:bg-green-600 text-white font-bold py-2 px-4 rounded"
           onClick={handleJoinRoom}
