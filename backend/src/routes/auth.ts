e7974c4adace1d7b39eb0b20a1b0be455e6a527b
uvsq22400158 <yanni.doumbia@ens.uvsq.fr>
2025-02-11 15:59:34 +0100

    Ajout de commentaires dans les fichiers utiliseés pour la connexion et l'inscription

--- backend/src/routes/auth.ts
+++ backend/src/routes/auth.ts
@@ -4,77 +4,80 @@ import prisma from "../prismaClient";
 
 const router = express.Router();
 
-// Inscription
+//Partie qui se charge de l'inscription 
 router.post("/register", async (req: Request, res: Response): Promise<void> => {
+    //Recupere les donnees de la requete 
     const { pseudo, email, mdp, mdp2 } = req.body;
-
+    //Essaye plusieurs verifications pour l'inscription et essaye de créer un utilisateur
     try {
-        // Vérifier si le mail existe déjà en bdd
+        //Cherche si un utilisateur avec le mail existe déjà en bdd
         const existingMail = await prisma.utilisateur.findUnique({
             where: { email }
         });
-
+        //Si le mail est deja utilisé, renvoi l'erreur vers le front
         if (existingMail) {
             res.status(400).json({ error: "Cet email est déjà utilisé." });
             return;
         }
-        // Vérifier si le pseudo existe déjà en bdd
+        //Cherche si un utilisateur avec le pseudo existe déjà en bdd
         const existingUser = await prisma.utilisateur.findUnique({
             where: { pseudo }
         });
-
+        //Si le pseudo est deja utilisé, renvoi l'erreur vers le front
         if (existingUser) {
             res.status(400).json({ error: "Ce nom d'utilisateur est déjà utilisé." });
             return;
         }
-
+        //Hache le mot de passe
         const hashedPassword = await bcrypt.hash(mdp, 10);
+        //Cree un utilisateur
         const user = await prisma.utilisateur.create({
             data: { pseudo, email, mdp_hash: hashedPassword },
         });
-
         res.json({ message: "Utilisateur inscrit avec succès", user });
+    //En cas d'erreur renvoi l'erreur d'inscription
     } catch (error) {
         console.error("Erreur Prisma :", error);
         res.status(500).json({ error: "Erreur d'inscription backend" });
     }
 });
 
-// Connexion
+//Partie qui se charge de la connexion
 router.post("/login", async (req: Request, res: Response): Promise<void> => {
+    //Recupere les donnees de la requete 
     const { pseudo, email, mdp } = req.body;
-
+    //Essaye de trouver l'user correspondant
     try {
-        // Vérifier si l'utilisateur existe avec ce pseudo
+        //Cherche si un utilisateur existe avec ce pseudo
         const existingPseudo = await prisma.utilisateur.findUnique({
             where: { pseudo },
         });
-        // Vérifier si l'utilisateur existe avec cet email
+        //Cherche si l'utilisateur existe avec cet email
         const existingMail = await prisma.utilisateur.findUnique({
             where: { email },
         });
-
+        //Si le login n'est pas attribué renvoi une erreu vers le front
         if (!existingMail && !existingPseudo) {
             res.status(400).json({ error: "Login ou mot de passe incorrect." });
             return;
         }
-        //donne a user l'objet représentant l'utilisateur
+        //Donne a user l'objet représentant l'utilisateur
         let user: any;
-
         if (existingPseudo) {
             user = existingPseudo;
         } else {
             user = existingMail;
         }
-        // Vérifier si le mot de passe est correct
+        //Vérifie si le mot de passe est correct
         const isPasswordValid = await bcrypt.compare(mdp, user.mdp_hash);
-
+        //Si le mdp est invalide envoi une erreur vers le front
         if (!isPasswordValid) {
             res.status(400).json({ error: "Login ou mot de passe incorrect." });
             return;
         }
 
         res.json({ message: "Connexion réussie", user });
+    //En cas d'erreur renvoi l'erreur de connexion
     } catch (error) {
         console.error("Erreur Prisma :", error);
         res.status(500).json({ error: "Erreur de connexion backend" });
@@ -82,6 +85,6 @@ router.post("/login", async (req: Request, res: Response): Promise<void> => {
 });
 
 
-
+  
 
 export default router;
--- frontend/src/pages/Home.tsx
+++ frontend/src/pages/Home.tsx
@@ -1,11 +1,15 @@
 import { Link } from "react-router-dom";
-
+import logo from '../assets/Logo_CodeNames_blanc.svg';
 export default function Home() {
+  //HTML utilisant Tailwind css de la page d'Accueil  
   return (
-    <div className="flex flex-col items-center justify-center min-h-screen bg-gray-100">
-      <h1 className="text-4xl font-bold text-blue-600">Bienvenue sur Codenames !</h1>
+    <div className="flex flex-col items-center justify-center min-h-screen bg-[#0a0f1f]">
+      <img src={logo} alt="Codenames Logo" className="w-42 h-40=2 mb-4" />
+      <h1 className="text-4xl font-bold text-blue-300">Bienvenue sur Codenames !</h1>
       <Link to="/login" className="mt-4 px-4 py-2 bg-blue-500 text-white rounded">Se Connecter</Link>
-      <Link to="/register" className="mt-2 px-4 py-2 bg-green-500 text-white rounded">S'inscrire</Link>
+      <Link to="/register" className="mt-2 px-4 py-2 bg-blue-500 text-white rounded">S'inscrire</Link>
+      <p className="mt-4 font-bold text-blue-300">À vous de décrypter le message !</p>
     </div>
+
   );
 }
