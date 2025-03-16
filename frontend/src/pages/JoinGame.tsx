ce1712fcec302d21b5c20810117b6d0fc3e37ed6
Edgar BAGHDASARYAN <edgar.baghdasaryan@ens.uvsq.fr>
2025-03-16 11:34:08 +0100

    modif import styles

--- frontend/src/pages/JoinGame.tsx
+++ frontend/src/pages/JoinGame.tsx
@@ -4,7 +4,7 @@ import { useNavigate } from "react-router-dom";
 import { getUtilisateur } from "../../utils/utilisateurs";
 import { getToken } from "../../utils/token";
 import "../index.css"
-import styles from "../styles/login.module.css";
+import styles from "../styles/Login.module.css";
 
 
 // import { io } from 'socket.io-client';
