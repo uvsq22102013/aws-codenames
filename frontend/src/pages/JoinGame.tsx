f3cf85770601a080ad437f8b68c132223df7a35e
Ayoub Boussaid <ayoub.boussaid@ens.uvsq.fr>
2025-03-25 13:27:50 +0100

    boutton

--- frontend/src/pages/JoinGame.tsx
+++ frontend/src/pages/JoinGame.tsx
@@ -217,7 +217,7 @@ const bouttonUtilisateur = () => {
               <div className="absolute top-full right-0 w-full bg-[#222] border border-yellow-400 rounded shadow-lg z-10 flex flex-col gap-2 p-2 items-center mt-1">
                 <button
                   onClick={deconnexion}
-                  className="w-auto break-words text-yellow-400 hover:text-white border border-yellow-400 hover:bg-yellow-500 focus:ring-4 focus:outline-none focus:ring-yellow-300 font-medium rounded-lg text-[10px] px-1 py-1 text-center mb-1 dark:border-yellow-300 dark:text-yellow-300 dark:hover:text-white dark:hover:bg-yellow-400 dark:focus:ring-yellow-900"
+                  className="w-auto break-words flex flex-wrap text-yellow-400 hover:text-white border border-yellow-400 hover:bg-yellow-500 focus:ring-4 focus:outline-none focus:ring-yellow-300 font-medium rounded-lg text-[10px] px-1 py-1 text-center mb-1 dark:border-yellow-300 dark:text-yellow-300 dark:hover:text-white dark:hover:bg-yellow-400 dark:focus:ring-yellow-900"
                 >
                   {texts[language].deco}
                 </button>
